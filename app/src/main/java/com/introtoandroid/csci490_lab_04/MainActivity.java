e576bd5cf75807d8942e4f560c1efcecf8681c22
jacobmattox <jacob.mattox1017@gmail.com>
2019-03-04 20:11:39 -0500

    url connection lab

--- app/src/main/java/com/introtoandroid/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/introtoandroid/csci490_lab_04/MainActivity.java
@@ -1,7 +1,18 @@
 package com.introtoandroid.csci490_lab_04;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println(methodNameUnknown("https://jsonplaceholder.typicode.com/users"));
+
+
+    }
+    public String methodNameUnknown(String s){
+
+        URL url = null;
+        try{
+            url = new URL(s);
+        }
+        catch(MalformedURLException e){
+            e.printStackTrace();
+        }
+        URLConnection urlConnection = null;
+        try{
+            urlConnection = url.openConnection();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        InputStreamReader iSReader = null;
+        try{
+            iSReader = new InputStreamReader(urlConnection.getInputStream());
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(iSReader);
+
+        String line = null;
+
+        try {
+            line = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.v("broken", line);
+
+        return "yourMother";
+
     }
 }
